5aa68ac4083a61140087192cf62eea233beea399
ayoadeleye <ayooluwaadeleye17@gmail.com>
2023-01-04 16:31:55 -0600

    some changes and a database now

--- flaskblog.py
+++ flaskblog.py
@@ -75,7 +75,5 @@ def login():
     return render_template("login.html", title='Login', form=form)
 
 
-with app.app_context():
-    db.create_all()
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
